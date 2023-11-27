def3adb59001ea841e75cfd7bd8bc0cc3ab67fb9
Foxxybb <62300241+Foxxybb@users.noreply.github.com>
2023-11-27 13:33:24 -0600

    Update Events.cs

--- Scripts/Autoload/Events.cs
+++ Scripts/Autoload/Events.cs
@@ -4,24 +4,17 @@ using System;
 public partial class Events : Node
 {
 	public static Events Instance;
-	// SINGLETON class that can be used as an event handler
+
 	// CUSTOM SIGNALS MUST END WITH "EventHandler"
 
 	[Signal] public delegate void GameSceneReadyEventHandler(); // Emitted by GameScene Script when _Ready()
 	[Signal] public delegate void PlayerStateChangeEventHandler(); // Emitted by Player when state text changes
 	
 	[Signal] public delegate void AnnouncerEventHandler(string phrase); // Emitted by: Oracle
-	//[Signal] public delegate void SoundEffectEventHandler(AudioStreamWav sound, Node2D node, float baseDB); // Emitted by: Objects that need to play sounds
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		Instance = this;
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-
 	}
 }
